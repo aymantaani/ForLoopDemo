1bee43edf0014efe0195fc46a0ef8cc9a2cc8a23
aymantaani <88418296+aymantaani@users.noreply.github.com>
2023-10-22 15:38:35 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ForLoopDemo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region standard for
+            for (int i = 1; i <= 10; i++)
+            {
+                Console.Write (i + " ");
+            }
+            Console.WriteLine();
+
+            #endregion
+
+            #region user-range for
+
+
+            #endregion
+
+        }
+    }
+}
